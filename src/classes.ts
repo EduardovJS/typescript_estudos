538d3c1518480b8edfc36e35c7ef24a6264e374f
EduardovJS <eduardoviniciusjorge@gmail.com>
2025-04-04 00:51:31 -0300

    classes

---
+++ src/classes.ts
@@ -0,0 +1,27 @@
+interface IPerso {
+  id: number;
+  sayMyName(): string;
+}
+
+class Perso implements IPerso {
+  readonly id: number;
+  protected name: string;
+  private age: number;
+
+  constructor(id: number, name: string, age: number) {
+    this.id = id;
+    this.name = name;
+    this.age = age;
+  }
+  sayMyName() {
+    return this.name;
+  }
+}
+
+class Employee extends Perso {
+  constructor(id: number, name: string, age: number) {
+    super(id, name, age);
+  }
+}
+
+const eduardo = new Perso(1, "Eduardo", 22);
